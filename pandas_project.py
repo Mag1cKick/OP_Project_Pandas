637ebd0e9470c51cc76e97024905898cc2ce6603
dismous <bohdanpopov9@gmail.com>
2023-12-04 10:59:23 +0200

    Update pandas_project.py

--- pandas_project.py
+++ pandas_project.py
@@ -2,6 +2,7 @@
 import pandas as pd
 
 def framing():
+    """creates pandas DataFrame"""
     file = '/Users/bohdanpopov/Documents/GitHub/OP_Project_Pandas/train.csv'
     df = pd.read_csv(file)
     return df
